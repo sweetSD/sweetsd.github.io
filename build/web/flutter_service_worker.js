96d070bd17fd49a50c0d21d062fcd38a47cdbaed
sweetSD <an060875@naver.com>
2022-09-05 15:18:08 +0900

    build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,8 +25,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f9a08a3e6d831eaf243e6df8e0654af3",
-"/": "f9a08a3e6d831eaf243e6df8e0654af3",
+"index.html": "ff272e2bba2ad9c55b0363a84e7ca313",
+"/": "ff272e2bba2ad9c55b0363a84e7ca313",
 "main.dart.js": "df16bb77e718e9d78223bb4faab0a32b",
 "manifest.json": "fb38684fab3f56cfd7dfe39d92756772",
 "version.json": "256d8a26bfc34aa2ff29a0a98a538b76"
